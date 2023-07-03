743d3ca6006084c3761f942808b6a30b4ee06680
yahia-salah <ysalah@integrant.com>
2023-07-03 22:53:27 +0300

    Cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions";
+import { db } from "./init";
+import { FieldValue } from "firebase-admin/firestore";
 
 /**
  * Import function triggers from their respective submodules:
@@ -12,7 +14,22 @@ import * as functions from "firebase-functions";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+export const onAddCourseUpdatePromoCounter = functions
+  .runWith({
+    timeoutSeconds: 300,
+    memory: "128MB",
+  })
+  .firestore.document("courses/{courseId}")
+  .onCreate(async (snap, context) => {
+    functions.logger.debug(
+      `Running add course trigger for courseId ${context.params.courseId}`
+    );
+
+    const course = snap.data();
+
+    if (course.promo) {
+      return db.doc("/course/stats").update({
+        totalPromo: FieldValue.increment(1),
+      });
+    }
+  });
---
+++ functions/src/init.ts
@@ -0,0 +1,7 @@
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+export const db = admin.firestore();
+
+export const auth = admin.auth();
